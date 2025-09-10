16a512647b6de86801450894559312be41047ddb
winner00kambale <vainqueurkambale@gmail.com>
2025-09-10 16:18:05 +0200

    final commit

--- resources/views/layouts/mobile-menu/mobile-menu.blade.php
+++ resources/views/layouts/mobile-menu/mobile-menu.blade.php
@@ -2,8 +2,8 @@
      <nav class="mobile-menu__wrapper">
          <div class="mobile-menu__header nav-fade">
              <div class="logo">
-                 <a href="index.html" aria-label="home page" title="logo">
-                     <img src="assets/images/logo.png" alt="Image">
+                 <a href="{{ '/' }}" aria-label="home page" title="logo">
+                     <img src="{{ asset('assets/images/logo.png') }}" alt="Image">
                  </a>
              </div>
              <button aria-label="close mobile menu" class="close-mobile-menu">
@@ -12,20 +12,20 @@
          </div>
          <div class="mobile-menu__list"></div>
          <div class="mobile-menu__cta nav-fade d-block d-md-none">
-             <a href="donate-us.html" class="btn--secondary" data-text="Donate Now"><span>Donate
-                     Now</span></a>
+             <a href="{{ route('contact.index') }}" class="btn--secondary" data-text="Contactez-nous"><span>Contactez-nous
+                     </span></a>
          </div>
          <div class="mobile-menu__social social nav-fade">
-             <a href="https://www.facebook.com/" target="_blank" aria-label="share us on facebook" title="facebook">
+             <a href="{{ optional($data)->facebook }}" target="_blank" aria-label="share us on facebook" title="facebook">
                  <i class="fa-brands fa-facebook-f"></i>
              </a>
-             <a href="https://vimeo.com/" target="_blank" aria-label="share us on vimeo" title="vimeo">
-                 <i class="fa-brands fa-vimeo-v"></i>
+             <a href="{{ optional($data)->instagram }}" target="_blank" aria-label="share us on vimeo" title="vimeo">
+                 <i class="fa-brands fa-instagram"></i>
              </a>
-             <a href="https://x.com/" target="_blank" aria-label="share us on twitter" title="twitter">
+             <a href="{{ optional($data)->twitter }}" target="_blank" aria-label="share us on twitter" title="twitter">
                  <i class="fa-brands fa-twitter"></i>
              </a>
-             <a href="https://www.linkedin.com/" target="_blank" aria-label="share us on linkedin" title="linkedin">
+             <a href="{{ optional($data)->linkedin }}" target="_blank" aria-label="share us on linkedin" title="linkedin">
                  <i class="fa-brands fa-linkedin-in"></i>
              </a>
          </div>
