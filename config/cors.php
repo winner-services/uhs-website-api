b886dcd09d1000741a6b49e820d0c2955ac82cb4
winner00kambale <vainqueurkambale@gmail.com>
2025-09-13 09:03:19 +0200

    final commit

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'https://app.uhs-rdc.org',
+        // 'https://app.uhs-rdc.org',
     ],
 
     'allowed_origins_patterns' => [],
