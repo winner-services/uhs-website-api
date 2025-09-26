f79d635225f66906347ac3975c453299d390c497
winner00kambale <vainqueurkambale@gmail.com>
2025-09-26 11:17:01 +0200

    don commit

--- resources/views/pages/don/don.blade.php
+++ resources/views/pages/don/don.blade.php
@@ -29,7 +29,7 @@
                 <div class="col-12 col-lg-10 col-xl-5">
                     <div class="section__header text-center mb-60" data-aos="fade-up" data-aos-duration="1000">
                         <span>Soutenir notre cause ensemble</span>
-                        <h4 class="title-animation">Faites un don aujourd'hui et aidez-nous à faire la différence !</h4>
+                        <h4 class="title-animation">Faites un don aujourd'hui et aidez-nous à faire la différence </h4>
                         <div class="icon-thumb justify-content-center">
                             <div class="icon-thumb-single">
                                 <span></span>
