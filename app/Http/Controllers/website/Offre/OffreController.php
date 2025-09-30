fcca08b8afacfa7e1d3a0a33dc0a62ef58e399ff
winner00kambale <vainqueurkambale@gmail.com>
2025-09-30 16:02:46 +0200

    offres commit

--- app/Http/Controllers/website/Offre/OffreController.php
+++ app/Http/Controllers/website/Offre/OffreController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\website\Offre;
 use App\Http\Controllers\Controller;
 use App\Models\About;
 use App\Models\Event;
+use App\Models\Offre;
 use Illuminate\Http\Request;
 
 class OffreController extends Controller
@@ -12,6 +13,7 @@ class OffreController extends Controller
     public function indexOffreData(){
         $data = About::first();
         $twoEvents = Event::latest()->take(2)->get();
-        return view('pages.offres.offre', compact('data','twoEvents'));
+        $offres = Offre::latest()->get();
+        return view('pages.offres.offre', compact('data','twoEvents','offres'));
     }
 }
