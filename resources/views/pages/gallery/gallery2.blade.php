ea5ecf78d411e48fcc7570c2c4f289582e23b6e5
winner00kambale <vainqueurkambale@gmail.com>
2025-10-09 10:08:12 +0200

    update commit

--- resources/views/pages/gallery/gallery2.blade.php
+++ resources/views/pages/gallery/gallery2.blade.php
@@ -1,78 +1,14 @@
 <div class="gallery">
     <div class="gallery__inner">
         <div class="gallery__slider">
-            <div class="gallery__single">
-                <img src="assets/images/gallery/one.png" alt="Image">
-                <a href="index.html">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-            </div>
-            <div class="gallery__single">
-                <img src="assets/images/gallery/two.png" alt="Image">
-                <a href="index.html">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-            </div>
-            <div class="gallery__single">
-                <img src="assets/images/gallery/three.png" alt="Image">
-                <a href="index.html">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-            </div>
-            <div class="gallery__single">
-                <img src="assets/images/gallery/four.png" alt="Image">
-                <a href="index.html">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-            </div>
-            <div class="gallery__single">
-                <img src="assets/images/gallery/five.png" alt="Image">
-                <a href="index.html">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-            </div>
-            <div class="gallery__single">
-                <img src="assets/images/gallery/six.png" alt="Image">
-                <a href="index.html">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-            </div>
-            <div class="gallery__single">
-                <img src="assets/images/gallery/one.png" alt="Image">
-                <a href="index.html">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-            </div>
-            <div class="gallery__single">
-                <img src="assets/images/gallery/two.png" alt="Image">
-                <a href="index.html">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-            </div>
-            <div class="gallery__single">
-                <img src="assets/images/gallery/three.png" alt="Image">
-                <a href="index.html">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-            </div>
-            <div class="gallery__single">
-                <img src="assets/images/gallery/four.png" alt="Image">
-                <a href="index.html">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-            </div>
-            <div class="gallery__single">
-                <img src="assets/images/gallery/five.png" alt="Image">
-                <a href="index.html">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-            </div>
-            <div class="gallery__single">
-                <img src="assets/images/gallery/six.png" alt="Image">
-                <a href="index.html">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-            </div>
+            @foreach ($gallery as $index => $item)
+                <div class="gallery__single">
+                    <img src="{{ asset('storage/' . $item->image) }}" alt="Image">
+                    <a href="#">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                </div>
+            @endforeach
         </div>
+
     </div>
-</div>
