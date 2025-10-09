f84cc978450322a5cb46ea97481d51cc3294e102
winner00kambale <vainqueurkambale@gmail.com>
2025-10-09 10:15:48 +0200

    update commit

--- resources/views/pages/gallery/gallery2.blade.php
+++ resources/views/pages/gallery/gallery2.blade.php
@@ -2,9 +2,12 @@
     <div class="gallery__inner">
         <div class="gallery__slider">
             @foreach ($gallery as $item)
-                <div class="gallery__single">
-                    <img src="{{ asset('storage/' . $item->image) }}" alt="Image">
-                    <a href="#">
+                <div class="gallery__single"
+                    style="position: relative; overflow: hidden; width: 300px; height: 300px; border-radius: 10px; display: inline-block; margin: 5px;">
+                    <img src="{{ asset('storage/' . $item->image) }}" alt="Image"
+                        style="width: 100%; height: 100%; object-fit: cover; object-position: center;">
+                    <a href="#"
+                        style="position: absolute; bottom: 10px; right: 10px; color: #fff; background: rgba(0,0,0,0.5); border-radius: 50%; padding: 8px; font-size: 18px; text-decoration: none;">
                         <i class="fa-brands fa-instagram"></i>
                     </a>
                 </div>
