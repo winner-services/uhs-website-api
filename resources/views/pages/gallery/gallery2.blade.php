bd541780352c9084874b6cd084a3174aef42c62e
winner00kambale <vainqueurkambale@gmail.com>
2025-10-10 10:18:02 +0200

    update commit

--- resources/views/pages/gallery/gallery2.blade.php
+++ resources/views/pages/gallery/gallery2.blade.php
@@ -3,7 +3,7 @@
         <div class="gallery__slider">
             @foreach ($gallery as $item)
                 <div class="gallery__single">
-                    <img src="{{ asset('storage/' . $item->image) }}" alt="Image">
+                    <img src="{{ asset('storage/' . $item->image) }}" alt="Image" style="width: 100%; height: 250px; object-fit: cover; border-radius: 8px;">
                     <a href="#">
                         <i class="fa-brands fa-instagram"></i>
                     </a>
